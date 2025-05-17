602f29f180d2aca957ac917d4fd8664fb02db78d
anamul hauqe <parvesmosarof2@gmail.com>
2025-05-17 16:31:33 +0600

    solved 6 problemas

--- Src/Assignment1.ts
+++ Src/Assignment1.ts
@@ -134,4 +134,35 @@ function processValue(value: string | number): number {
 
 
 
+// problem : 6
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+    if (products.length === 0 ){
+        return null;
+    }
+    let mostExpensiveProduct = products[0];
+    products.forEach((product) => {
+        if (product.price > mostExpensiveProduct.price) {
+            mostExpensiveProduct = product;
+        }
+    })
+    return mostExpensiveProduct;
+}
+
+// Example usage:
+const products = [
+  { name: "Pen", price: 10 },
+  { name: "Notebook", price: 25 },
+  { name: "Bag", price: 50 }
+];
+
+console.log(getMostExpensiveProduct(products));
+
+
+
+
 
