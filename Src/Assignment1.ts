677a7d18a81f52a7986496ecd35979c47d2d3dc0
anamul hauqe <parvesmosarof2@gmail.com>
2025-05-17 16:01:16 +0600

    solved 4 problems

--- Src/Assignment1.ts
+++ Src/Assignment1.ts
@@ -78,5 +78,43 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
 
 
 // problem : 4
+class Vehicle {
+    private make: string;
+    private year: number;
+    constructor(make: string, year: number) {
+        this.make = make;
+        this.year = year;
+        }
+    getInfo(): string {
+        return `Make: ${this.make}, Year: ${this.year}`;
+        }
+}
+
+class Car extends Vehicle {
+    private model: string;
+    constructor(make: string, year: number, model: string) {
+        super(make, year);
+        this.model = model;
+        }
+        getModel(): string {
+        return `Model: ${this.model}`;
+        }
+    }
+// Example usage:
+    const myCar = new Car("Toyota", 2020, "Corolla");
+    const myCar2 = new Car("Honda", 2021, "Civic");
+
+    console.log(myCar.getInfo());  
+    console.log(myCar.getModel()); 
+
+
+
+
+
+
+
+
+
+// problem : 4
 
 
